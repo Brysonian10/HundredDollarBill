c965083971b39632e47a890936465db0f9b20537
Bryson Spaulding <brysonian10@hotmail.com>
2018-12-11 08:54:38 -0700

    honestly right now I'm so confused but I'm starting to get the hang of this whole coding thing. Right now I'm just trying to figure out the whole animation and gesture things

--- HundredDollarBill/Controller/ViewController.swift
+++ HundredDollarBill/Controller/ViewController.swift
@@ -15,38 +15,68 @@ class ViewController: UIViewController {
         
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
         let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
-        
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+
         leftSwipe.direction = .left
         rightSwipe.direction = .right
-        
+        upSwipe.direction = .up
+        downSwipe.direction = .down
+
         view.addGestureRecognizer(leftSwipe)
         view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(downSwipe)
     }
-    
+
     @objc func handleSwipes(_ sender:UISwipeGestureRecognizer)
     {
         if (sender.direction == .left)
         {
             print("Swipe Left")
-            
-            // show the view from the right side
+
+            // moves it left
         }
-        
+
         if (sender.direction == .right)
         {
             print("Swipe Right")
-            
-            // show the view from the left side
-        }
-        
-        
-        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-        
-        let resultViewController = storyBoard.instantiateViewController(withIdentifier: "Bill")
-        
-        self.present(resultViewController, animated:true, completion:nil)
-        
 
+            // moves it right
+        }
+
+        if (sender.direction == .up)
+        {
+            print("Swipe Up")
+
+            //moves it up
+        }
+
+        if (sender.direction == .down)
+        {
+            print("Swipe Down")
+
+            //moves it down
+        }
+
+
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+
+        let resultViewController = storyBoard.instantiateViewController(withIdentifier: "Bill")
+
+        self.present(resultViewController, animated:true, completion:nil)
 
     }
+    
+   private func animate()
+    {
+        //I want and If statement, if touching, then move hundred dollar bill to finger position. If at least 50 pixels from center of screen, slide off of screen, else, slide back to the center of the screen.
+    }
+    
+    private func counter()
+    {
+        //for every bill thrown away the counter label will go up a single number. This allows the user to know how many dollars they have wasted times 100.
+    }
+    
+    
 }
